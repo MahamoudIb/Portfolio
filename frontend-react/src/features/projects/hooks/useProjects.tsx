8465a55968f3d9dfddf081f7f052bd012440ad48
Mahamoud Ibrahim <mahamod.ibrahim@gmail.com>
2024-10-26 01:10:31 +0200

    la til resterende kode i page, ved å knytte den til projects komponenten

--- frontend-react/src/features/projects/hooks/useProjects.tsx
+++ frontend-react/src/features/projects/hooks/useProjects.tsx
@@ -87,3 +87,5 @@ export function useProjects() {
         },
       };
 }
+
+export default useProjects;
--- frontend-react/src/features/projects/pages/index.tsx
+++ frontend-react/src/features/projects/pages/index.tsx
@@ -1,3 +1,31 @@
+import Projects from "../components/Projects";
+import useProjects from "../hooks/useProjects";
+import type { HandleOptions } from "../types";
+
+
 export default function ProjectPage() {
-    
+    const { add, remove, status, data, error } = useProjects();
+    const projects = data;
+
+    const handleProjectOptions: HandleOptions = (props) => {
+        const { action } = props;
+
+        switch (action) {
+          case "add":
+            add(props.project);
+            break;
+          case "remove":
+            remove(props.id);
+            break;
+          default:
+            break;
+        };
+    }
+
+    if (status.loading) return <p>Prosjektene laster ...</p>;
+    if (status.error) return <p className="error">{error}</p>;
+
+   return (
+    <Projects projects={projects} handleProjectOptions={handleProjectOptions}/>
+   ) 
 }
