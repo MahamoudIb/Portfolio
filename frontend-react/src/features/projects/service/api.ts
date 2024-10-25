3b605103bb7ac1be5af2e425b9547f965c50f2cd
Mahamoud Ibrahim <mahamod.ibrahim@gmail.com>
2024-10-25 19:16:41 +0200

    La til config for URL endepunkter, og en API for http requests

---
+++ frontend-react/src/features/projects/service/api.ts
@@ -0,0 +1,54 @@
+import { ofetch } from "ofetch";
+
+import { endpoint } from "@/config/url";
+import { Project } from "../types";
+import { validateProject } from "../helpers/schema";
+
+const url = endpoint.project
+
+const remove = async (id: string) => {
+    try {
+        await ofetch(`${url}/${id}`, {
+            method: "DELETE"
+        });  
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
+
+const create = async (data: Omit<Project, 'id' |'publishedAt' | 'updatedAt'>) => {
+    try {
+        const createdProject = await ofetch(url, {
+            method: "POST",
+            body: data
+        });
+
+        return createdProject
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const getProjects = async () => {
+    try {
+        const projects = await ofetch(url);
+        return validateProject(projects.data)
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const update = async (id: string, data: Partial<Project>) => {
+    try {
+        await ofetch(`${url}/${id}`, {
+            method: "PATCH",
+            body: data
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export default { remove, create, update };
+
