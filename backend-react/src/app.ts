ff688c2fc9a2fbee70125a3d7e3dbdbf5ab5ad31
Mahamoud Ibrahim <mahamod.ibrahim@gmail.com>
2024-10-31 03:26:02 +0100

    Lagde kontroller

--- backend-react/src/app.ts
+++ backend-react/src/app.ts
@@ -1,42 +1,17 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import { projects } from "./data/projects";
-import { Project } from "./types"
+import { ProjectController } from "./features/projects/controller";
 
+export const makeApp = (
+) => {
+  const app = new Hono();
+  app.use("/*", cors());
 
-const app = new Hono();
+  ProjectController();
 
-app.use("/*", cors());
+  return app;
+};
 
-app.get("/v3/projects", async (c) => {
-  // await new Promise((resolve) => setTimeout(resolve, 3000));
-  return c.json({
-    data: projects,
-  });
-});
-
-app.post("/v3/projects", async (c) => {
-  const dataFromFrontend = await c.req.json();
-
-  const created : Project = dataFromFrontend
-  created.publishedAt = new Date();
-  created.id = crypto.randomUUID();
-
-  projects.push(created);
-  console.log(created)
-
-  return c.json(created, 201);
-});
-
-app.delete("/v3/projects/:id", (c) => {
-  const id = c.req.param("id");
-  const index = projects.findIndex((h) => h.id === id);
-
-  if (index === -1) {
-    return c.json(undefined, 404);
-  }
-  projects.splice(index, 1);
-  return c.json(undefined, 204);
-});
+const app = makeApp();
 
 export default app;
